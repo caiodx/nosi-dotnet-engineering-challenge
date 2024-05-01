e776e3bf68a3bb4c8cafcc5697bb1e0b1a7d3a8d
Caio SOuza <caiodx@gmail.com>
2024-05-01 23:32:36 +0100

    Fleunt Assertions

---
+++ NOS.Engineering.Challenge.Tests/UtilsTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace NOS.Engineering.Challenge.Tests
+{
+    public class UtilsTest
+    {
+
+    }
+}
