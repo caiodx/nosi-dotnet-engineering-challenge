f87c0ca8c18f430392b3a990de9f9ccca15973e3
Caio SOuza <caiodx@gmail.com>
2024-05-01 23:40:22 +0100

    Utils Test - Enviroment

--- NOS.Engineering.Challenge.Tests/UtilsTest.cs
+++ NOS.Engineering.Challenge.Tests/UtilsTest.cs
@@ -4,11 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using FluentAssertions;
+using NOS.Engineering.Challenge.Utils;
 
 namespace NOS.Engineering.Challenge.Tests
 {
     public class UtilsTest
     {
-
+        [Fact]
+        public void Enviroment_Should_Development_or_Production()
+        {
+            Enums.GetAppEnviroment().Should().BeOneOf([Enums.AppEnviroment.Production, Enums.AppEnviroment.Development]);
+        }
     }
 }
