801bdde5ca519dad7d06b4fe9df1dff1c5031c76
Caio SOuza <caiodx@gmail.com>
2024-05-02 02:15:10 +0100

    Database Unit Testes

---
+++ NOS.Engineering.Challenge.API.Tests/Controller.cs
@@ -0,0 +1,33 @@
+﻿using NOS.Engineering.Challenge.Managers;
+using FakeItEasy;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NOS.Engineering.Challenge.API.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NOS.Engineering.Challenge.API.Tests
+{
+    public class Controller
+    {
+        private readonly IContentsManager _manager;
+        public Controller()
+        {
+            _manager = A.Fake<IContentsManager>();
+        }
+
+        [Fact]
+        public void ContentController_GetManyContents_ReturnOK()
+        {
+            var controller = new ContentController(_manager);
+            var result = controller.GetManyContents().GetAwaiter().GetResult();
+
+            result.Should().NotBeNull();
+            result.Should().BeOfType(typeof(OkObjectResult));
+
+        }
+    }
+}
---
+++ NOS.Engineering.Challenge.Tests/MongoRepositoryTests.cs
@@ -0,0 +1,64 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using Moq;
+using NOS.Engineering.Challenge.Database;
+using NOS.Engineering.Challenge.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NOS.Engineering.Challenge.Tests
+{
+    public class MongoRepositoryTests
+    {        
+        private readonly IConfiguration _configuration;
+        private readonly IMapper<Content, ContentDto> _contetMapper;
+        public MongoRepositoryTests()
+        {
+            var configBuilder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json");
+
+            _configuration = configBuilder.Build();
+            _contetMapper = new ContentMapper();            
+        }
+
+        [Fact]
+        public void Repository_ReadAll_Should_Return_All()
+        {
+            var mongoDatabase = new MongoDatabase<Content, ContentDto>(_configuration, _contetMapper!);
+            var filters = FilterDefinition<Content>.Empty;
+
+            IEnumerable<Content?> result = mongoDatabase.ReadAll(filters).GetAwaiter().GetResult();
+
+            result.Should().NotBeNull();
+            result.Should().BeOfType<List<Content?>>();
+        }
+
+        [Fact]
+        public void Repository_Read_Should_Return_One_Or_Null()
+        {
+            var mongoDatabase = new MongoDatabase<Content, ContentDto>(_configuration, _contetMapper!);
+            //var filters = Builders<Content>.Filter.Eq(x => x.Id, Guid.Empty);
+            var filters = Builders<Content>.Filter.Eq(x => x.Id, Guid.Parse("8e7c6291-9ab3-47a8-8b27-069ec3fc151d"));
+
+            Content? result = mongoDatabase.Read(filters).GetAwaiter().GetResult();
+
+            if (result != null)
+            {
+                result.Should().BeOfType<Content?>();
+            }
+            else
+            {
+                result.Should().BeNull();
+            }
+           
+            
+            
+        }
+
+    }
+}
