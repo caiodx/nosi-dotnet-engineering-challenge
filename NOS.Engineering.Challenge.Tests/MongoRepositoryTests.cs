2c0664e07f208dfd9a556619fe1172cc0310382b
Caio SOuza <caiodx@gmail.com>
2024-05-02 20:09:08 +0100

    Teste Unitários Database

--- NOS.Engineering.Challenge.Tests/MongoRepositoryTests.cs
+++ NOS.Engineering.Challenge.Tests/MongoRepositoryTests.cs
@@ -1,9 +1,12 @@
 ﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.Configuration;
+using MongoDB.Bson;
 using MongoDB.Driver;
 using Moq;
 using NOS.Engineering.Challenge.Database;
 using NOS.Engineering.Challenge.Models;
+using StackExchange.Redis;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -59,15 +62,67 @@ namespace NOS.Engineering.Challenge.Tests
         }
 
         [Fact]
-        public void Repository_Create_Should_Return_Created_Item_Object()
+        public void Repository_Should_Create_And_Return_Created_Item_Object()
         {
             var mongoDatabase = new MongoDatabase<Content, ContentDto>(_configuration, _contetMapper!);
-            var newContent = new ContentDto("Title Test", "SubTitle Test", "Description Test", null, null, null, null, []);
+            var newContent = new ContentDto("Title Create", "SubTitle Create", "Description Create", null, null, null, null, []);
             var result = mongoDatabase.Create(newContent).GetAwaiter().GetResult();
 
             result.Should().NotBeNull();
             result.Should().BeOfType<Content?>();
         }
+
+        [Fact]
+        public void Repository_Should_Update_And_Return_Updated_Item_Object()
+        {
+            var mongoDatabase = new MongoDatabase<Content, ContentDto>(_configuration, _contetMapper!);
+            Guid id = Guid.Parse("8e7c6291-9ab3-47a8-8b27-069ec3fc151d");
+            var filters = Builders<Content>.Filter.Eq(x => x.Id, id);
+            var updatedContent = new ContentDto("Title Update", "SubTitle Update", "Description Update", null, null, null, null, []);
+
+            Content? result = mongoDatabase!.Update(updatedContent, filters!).GetAwaiter().GetResult();
+
+            if (result != null)
+            {
+                result.Should().BeOfType<Content?>();
+            }
+            else
+            {
+                result.Should().BeNull();
+            }
+        }
+
+        [Fact]
+        public void Repository_Should_Delete_And_Return_True_Or_False()
+        {
+            var mongoDatabase = new MongoDatabase<Content, ContentDto>(_configuration, _contetMapper!);
+            Guid id = Guid.Parse("8e7c6291-9ab3-47a8-8b27-069ec3fc151d");
+            var filters = Builders<Content>.Filter.Eq(x => x.Id, id);
+
+            var result = mongoDatabase!.Delete(filters!).GetAwaiter().GetResult();
+
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Repository_Should_Update_And_Return_True_Or_False() {
+            var mongoDatabase = new MongoDatabase<Content, ContentDto>(_configuration, _contetMapper!);
+            Guid id = Guid.Parse("8e7c6291-9ab3-47a8-8b27-069ec3fc151d");
+            var filters = Builders<Content>.Filter.Eq(x => x.Id, id);
+
+            Content? updateContent = mongoDatabase.Read(filters).GetAwaiter().GetResult();
+
+            if (updateContent != null)
+            {
+                var result = mongoDatabase.Update(updateContent!, filters).GetAwaiter().GetResult();
+                if (result)
+                    result.Should().BeTrue();
+                else
+                    result.Should().BeFalse();
+            }
+           
+        }
+
 
     }
 }
